c98e2196066753d329e95f821a0957522e162bc2
Alison G. Martins <odevalison@gmail.com>
2025-10-03 20:18:32 -0300

    feat(controller): implement order deletion

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,7 @@ import uploadConfig from "./config/multer.js";
 import multer from "multer";
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController.js";
 import { CreateOrderController } from "./controllers/order/CreateOrderController.js";
+import { DeleteOrderController } from "./controllers/order/DeleteOrderController.js";
 
 const router = Router();
 const upload = multer(uploadConfig.upload("tmp"));
@@ -38,5 +39,6 @@ router.post(
 router.get("/products", isAuthenticated, new ListByCategoryController().handle);
 
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
+router.delete("/order", isAuthenticated, new DeleteOrderController().handle);
 
 export { router };
