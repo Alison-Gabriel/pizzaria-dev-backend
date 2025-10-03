bd99be1a8c9fef262e2457ad6d27408ce83929d0
Alison G. Martins <odevalison@gmail.com>
2025-10-03 01:43:28 -0300

    feat: get user by id and return to controller

--- src/services/user/DetailUserService.ts
+++ src/services/user/DetailUserService.ts
@@ -1,8 +1,12 @@
 import prismaClient from "../../prisma/index.js";
 
 class DetailUserService {
-  async execute() {
-    return { ok: true };
+  async execute(user_id: string) {
+    const user = await prismaClient.user.findFirst({
+      where: { id: user_id },
+      select: { id: true, name: true, email: true },
+    });
+    return user;
   }
 }
 
