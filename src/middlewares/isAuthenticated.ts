ee6f6b27f77a4a65c8cea95797ee6d245382f1bd
Alison G. Martins <odevalison@gmail.com>
2025-10-03 01:38:38 -0300

    feat: ensure middleware verify if user is authenticated

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,10 +1,29 @@
 import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface Payload {
+  sub: string;
+}
 
 export const isAuthenticated = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log({ message: "Called a middleware" });
-  return next();
+  const authToken = req.headers.authorization;
+  if (!authToken) {
+    return res.status(401).end();
+  }
+
+  const token = authToken.replace(/\s?Bearer\s?/, "");
+
+  try {
+    const { sub } = jwt.verify(token, process.env.JWT_SECRET) as Payload;
+
+    req.user_id = sub;
+
+    return next();
+  } catch (err) {
+    return res.status(400).end();
+  }
 };
