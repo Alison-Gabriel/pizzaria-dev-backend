7ec70361e46728fd9e2388471c7f3c60739c01b9
Alison G. Martins <odevalison@gmail.com>
2025-10-03 18:54:30 -0300

    feat(upload): serve tmp folder as static

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,19 @@ import express, {
   type Response,
 } from "express";
 import { router } from "./routes.js";
+import path from "node:path";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
+
+app.use(
+  "/files",
+  express.static(path.resolve(import.meta.dirname, "..", "tmp"))
+);
+
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
     return res.status(400).json({ error: err.message });
