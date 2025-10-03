9b10c075eaedf9aaf57b44ace1310a9a89be2a7e
Alison G. Martins <odevalison@gmail.com>
2025-10-03 00:49:21 -0300

    feat: add initial DetailUserController

---
+++ src/controllers/user/DetailUserController.ts
@@ -0,0 +1,13 @@
+import type { Request, Response } from "express";
+import { DetailUserService } from "../../services/user/DetailUserService.js";
+
+class DetailUserController {
+  async handle(req: Request, res: Response) {
+    const detailUserService = new DetailUserService();
+    const user = await detailUserService.execute();
+
+    return res.json(user);
+  }
+}
+
+export { DetailUserController };
