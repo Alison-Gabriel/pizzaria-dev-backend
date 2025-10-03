319f56a7ab294c9254b4ac6eb46ecef42105a13c
Alison G. Martins <odevalison@gmail.com>
2025-10-03 02:25:35 -0300

    feat(controller): implement CreateCategory

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,20 @@ import { CreateUserController } from "./controllers/user/CreateUserController.js
 import { AuthUserController } from "./controllers/user/AuthUserController.js";
 import { DetailUserController } from "./controllers/user/DetailUserController.js";
 import { isAuthenticated } from "./middlewares/isAuthenticated.js";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController.js";
 
 const router = Router();
 
 // User routes
-router.post("/users", new CreateUserController().handle);
+router.post("/user", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+// Category routes
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
 
 export { router };
