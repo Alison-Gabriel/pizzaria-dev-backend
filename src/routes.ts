0465cef66cce14618f9fdd60316a3a39a507f3a0
Alison G. Martins <odevalison@gmail.com>
2025-10-03 23:13:41 -0300

    feat(route): add conclude order

--- src/routes.ts
+++ src/routes.ts
@@ -16,16 +16,15 @@ import { DeleteItemController } from "./controllers/order/DeleteItemController.j
 import { FinishOrderController } from "./controllers/order/FinishOrderController.js";
 import { ListOrdersController } from "./controllers/order/ListOrdersController.js";
 import { OrderDetailsController } from "./controllers/order/OrderDetailsController.js";
+import { ConcludeOrderController } from "./controllers/order/ConcludeOrderController.js";
 
 const router = Router();
 const upload = multer(uploadConfig.upload("tmp"));
 
-// User
 router.post("/user", new CreateUserController().handle);
 router.post("/auth", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
 
-// Category
 router.post(
   "/category",
   isAuthenticated,
@@ -37,7 +36,6 @@ router.get(
   new ListCategoriesController().handle
 );
 
-// Product
 router.get("/products", isAuthenticated, new ListByCategoryController().handle);
 router.post(
   "/product",
@@ -46,7 +44,6 @@ router.post(
   new CreateProductController().handle
 );
 
-// Order
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
 router.delete("/order", isAuthenticated, new DeleteOrderController().handle);
 router.get("/orders", isAuthenticated, new ListOrdersController().handle);
@@ -55,9 +52,12 @@ router.patch(
   isAuthenticated,
   new FinishOrderController().handle
 );
-router.patch("/order/conclude");
+router.patch(
+  "/order/conclude",
+  isAuthenticated,
+  new ConcludeOrderController().handle
+);
 
-// Order => Item
 router.post("/order/item", isAuthenticated, new AddItemController().handle);
 router.delete(
   "/order/item",
